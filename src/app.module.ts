6f0ca3dfbba96aaf8608e8b864ed27cf17ee78d3
Muhammad Zuhrul Umam <zuhrulu@gmail.com>
2018-09-18 20:59:21 +0700

    add mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { MongooseModule } from "@nestjs/mongoose";
+
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    MongooseModule.forRoot('mongodb://nestmodules:nestmodules@localhost:27018/nestmodules')
+  ],
+  controllers: [],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
